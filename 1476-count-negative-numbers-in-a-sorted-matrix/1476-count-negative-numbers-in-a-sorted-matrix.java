a4eb4414f219f8f07e7dabc9cebda0e1344fc529
Sameer K Bhardwaj <104968393+bharsa09@users.noreply.github.com>
2023-10-20 21:23:20 +0530

    Time: 0 ms (100%), Space: 43.5 MB (93.24%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.java
@@ -4,17 +4,25 @@ class Solution {
         int rowStart=0;
         int rowEnd=grid.length-1;
         int res=0;
+        int lastNeg=grid[0].length-1;
 
         while(rowStart<=rowEnd){
 
+            //edge case==>1
             if(grid[rowStart][0]<0){
                 res=res+grid[0].length;
                 rowStart++;
                 continue;
             }
 
+            //edge case==>2
+            if(grid[rowStart][grid[0].length-1]>0){
+                rowStart++;
+                continue;
+            }
+
             int low=0;
-            int high=grid[0].length-1;
+            int high=lastNeg;
             while(low<=high){
                 int mid=(low+high)/2;
 
@@ -25,6 +33,14 @@ class Solution {
                 }
             }
             res=res+grid[0].length-low;
+            
+            //next iteration of the row is last negative number found which is 'low'
+            //in previous row because columns are sorted and non-decreasing
+            //if no negative number present then low will be equal to length of row
+            //so it will give out of bound error in next iteration 
+            //so we have to put check point above mentioned in edge case number 2
+            lastNeg=low;
+
             rowStart++;
 
         }
