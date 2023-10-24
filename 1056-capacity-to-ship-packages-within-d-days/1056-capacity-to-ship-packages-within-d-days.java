96ca4ba60584985e49c4bc40c6f95116350784fc
Sameer K Bhardwaj <104968393+bharsa09@users.noreply.github.com>
2023-10-24 21:08:02 +0530

    Time: 10 ms (77.21%), Space: 44.8 MB (77.18%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -7,19 +7,30 @@ class Solution {
             totalWeight+=w;
             maxWeight=Math.max(maxWeight,w);
         }
+        //why we have taken this as maxweight?
+        /*
+
+        take this example [1,2,3,1,1].   if we take low as 0
+
+        1st iteration mid===>4
+        2nd itertion mid=2. Here we have issue because 3 in belt will never albe to load
+        so we have to take that weight so all the weight can be loaded one by one in worst case
+        so we choose the max weight in the array
+
+        */
 
         int low=maxWeight;
         int high=totalWeight;
 
-        
-
         while(low<=high){
             int mid=(low+high)/2;
-
             int d=calculateDays(weights,mid);
-            //System.out.println(mid+" "+d);
 
             if(d<=days){
+                //while adjusting high and low be carefull 
+                //if calculatedDays is lesser than given date or equal then it means we have taken
+                //min weight is very large thats why calculate day is less
+                //so we have to reduce the weight
                 high=mid-1;
             }else{
                 low=mid+1;
@@ -32,9 +43,7 @@ class Solution {
     public int calculateDays(int[] weights, int kg){
 
         int days=1;
-
         int sum=0;
-
         for(int i=0;i<weights.length;i++){
             if(sum+weights[i]>kg){
                 days++;
